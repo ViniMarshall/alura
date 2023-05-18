819d658827c6afd4b70e32ae1104493137b31d05
Vinicius Barbo <vinicius.barbo@restoque.com.br>
2023-05-18 08:39:15 -0300

    [react] Aula 2023-05-18

--- react/alura-studies/src/App.tsx
+++ react/alura-studies/src/App.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
+import Formulario from './components/form';
+import Lista from './components/list';
 
 function App() {
   return (
     <div className="App">
+      <Formulario />
+      <Lista />
     </div>
   );
 }
